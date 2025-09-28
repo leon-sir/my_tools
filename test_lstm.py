92eab353e367894668f28c91a2a7bab57e10ebca
hasee_windows <736262958@qq.com>
2025-09-28 09:52:30 +0800

    feat: add lpv data for lstm testing

--- test_lstm.py
+++ test_lstm.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import numpy as np
 import torch.optim as optim
 from matplotlib import pyplot as plt
+import os
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 input_size = 1  # 输入变量的维度，一维
 batch_size = 1
